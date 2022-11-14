83558a9413a2d01eb1c6a4315b51550497dbe28b
Armando Beltran <armando_b@live.com>
2022-11-14 16:18:14 -0600

    Defined Unprotected GET Request in Replies Views App File

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Reply
+from .serializers import ReplySerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_replies(request):
+    replies = Reply.objects.all()
+    serializer = ReplySerializer(replies, many=True)
+    return Response(serializer.data)
