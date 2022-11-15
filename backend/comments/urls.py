6a2906b5db08b1d0e5903ce3f7d9cb491913bfec
Shane <shanetanner03@gmail.com>
2022-11-15 13:39:30 -0600

    Wrote logic for PUT, GET by id, and DELETE for Comments. + Updated urls for Comments.

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -4,4 +4,6 @@ from comments import views
 urlpatterns = [
     path('', views.user_comments),
     path('all/', views.get_all_comments),
+    path('<int:pk>/', views.comment_detail),
+
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -29,4 +30,19 @@ def user_comments(request):
         comments = Comment.objects.filter(user_id=request.user.id)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)    
-    
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def comment_detail(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
